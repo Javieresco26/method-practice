ce2f2138aae8bea3204571868bd95ebb0c205028
alexr <alexramirez92606@gmail.com>
2026-04-13 13:25:33 -0500

    Menu

---
+++ src/main/java/com/pluralsight/Menu.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class Menu {
+    public static void main(String[] args) {
+        displayMenu();
+        displayMenu();
+    }
+
+    public static void displayMenu() {
+        System.out.println("=== MENU ===");
+        System.out.println("1. Coffe - $3.99");
+        System.out.println("2. Tea - $2.99");
+        System.out.println("3. Cookie - $1.99");
+
+    }
+
+}
